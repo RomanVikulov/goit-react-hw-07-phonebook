746b7919bc75a49dfa3c0f848193fbd5f3f585d7
Roman <romanuch06.88@gmail.com>
2022-12-05 12:08:17 +0200

    Create Filter.styled.js

---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { Field, Label, Input } from '../Form/Form.styled';
+
+const FieldFilter = styled(Field)``;
+
+const LabelFilter = styled(Label)`
+  font-size: 14px;
+`;
+
+const InputFilter = styled(Input)`
+  width: 200px;
+  padding: 5px;
+`;
+
+export { FieldFilter, LabelFilter, InputFilter };
