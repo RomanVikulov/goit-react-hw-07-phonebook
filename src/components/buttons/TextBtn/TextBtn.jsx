d9a54025722b1f4f48cf598b3fbded68040f2266
Roman <romanuch06.88@gmail.com>
2022-12-07 11:54:50 +0200

    Create TextBtn.jsx

---
+++ src/components/buttons/TextBtn/TextBtn.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TextBtn from './TextBtn.styled';
+
+const TextButton = ({ text, type, onClick, ...allyProps }) => (
+  <TextBtn type={type} onClick={onClick} {...allyProps}>
+    {text}
+  </TextBtn>
+);
+
+TextButton.defaultProps = {
+  onClick: () => null,
+  text: null,
+};
+
+TextButton.propTypes = {
+  onClick: PropTypes.func,
+  text: PropTypes.string,
+  type: PropTypes.string.isRequired,
+};
+
+export default TextButton;
