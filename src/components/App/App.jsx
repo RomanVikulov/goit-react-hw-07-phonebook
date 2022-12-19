afd660a0424dcdb6a7803cbdba2ed4fefdf285c8
Roman <romanuch06.88@gmail.com>
2022-12-19 21:10:54 +0200

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,24 @@
+import { ContactForm } from '../Form/Form';
+import { ContactList } from '../List/List';
+import { Filter } from '../Filter/Filter';
+import { Container, Section, Title } from '../base/CommonStyle.styled';
+import { GlobalStyle } from 'components/base/GlobalStyle';
+
+const App = () => {
+  return (
+    <Container>
+      <Section>
+        <Title as="h1">Phonebook</Title>
+        <ContactForm />
+      </Section>
+      <Section>
+        <Title as="h2">Contacts</Title>
+        <Filter />
+        <ContactList />
+      </Section>
+      <GlobalStyle />
+    </Container>
+  );
+};
+
+export default App;
