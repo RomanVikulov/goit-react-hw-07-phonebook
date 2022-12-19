54acee0be9d914fe00eb14590c8f16b0a099a227
Roman <romanuch06.88@gmail.com>
2022-12-19 21:10:56 +0200

    Create CommonStyle.styled.js

---
+++ src/components/base/CommonStyle.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: grid;
+  justify-content: center;
+  grid-gap: 20px;
+  width: 400px;
+  margin: 30px auto;
+`;
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 30px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.75);
+`;
+
+const Title = styled.h1`
+  margin-bottom: 20px;
+  font-size: 32px;
+  font-weight: 700;
+  color: rgb(0, 0, 0);
+`;
+
+export { Container, Section, Title };
