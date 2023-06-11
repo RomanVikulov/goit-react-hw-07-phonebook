8193fd741e20a5e96c2e66dff7ac4b4e3b304b6b
Roman <vikulov.roman.work@gmail.com>
2023-06-11 14:47:40 +0300

    Update Item.styled.js

--- src/components/Item/Item.styled.js
+++ src/components/Item/Item.styled.js
@@ -1,2 +1,12 @@
-export { Item, Name, Number, DeleteIconBtn };
+import styled from 'styled-components';
+
+const Item = styled.li`
+  font-size: 16px;
+  font-weight: 500;
+  color: #2a2a2a;
+  &:not(:last-child) {
+    margin-bottom: 10px;
+  }
+`;
+
 export { Item };
