198829969e8758e69373efc4d1feb615dbc42a07
Roman <vikulov.roman.work@gmail.com>
2023-06-11 14:47:42 +0300

    Update List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -29,30 +29,7 @@ export const ContactList = () => {
 
       {isLoading && !error && <Notify>Loading contacts...</Notify>}
       {error && <Notify>{error}</Notify>}
-import { ContactItem } from 'components/Item/Item';
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { getContacts, getFilter } from 'redux/selectors';
-import { List } from './List.styled';
 
-export const ContactList = () => {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-
-  const getVisibleContacts = () => {
-    return contacts.filter(({ name }) => name.toLowerCase().includes(filter));
-  };
-  const visibleContacts = getVisibleContacts();
-
-  return (
-    <List>
-      {visibleContacts.length === 0 && <div>No contacts</div>}
-      {visibleContacts.map(({ id, name, number }) => (
-        <ContactItem key={id} id={id} name={name} number={number} />
-      ))}
-    </List>
-  );
-};
       {contacts.length === 0 && !error && !isLoading && (
         <Notify>No contacts</Notify>
       )}
