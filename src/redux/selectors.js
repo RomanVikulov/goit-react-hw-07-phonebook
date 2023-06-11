05cae81c962a6dc348a8da14d91f8b39bfe4d518
Roman <vikulov.roman.work@gmail.com>
2023-06-11 15:17:01 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,7 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getContacts = state => state.contacts;
 export const getFilter = state => state.filter;
-import { createSelector } from '@reduxjs/toolkit';
 
 export const selectContacts = state => state.contacts.items;
 
