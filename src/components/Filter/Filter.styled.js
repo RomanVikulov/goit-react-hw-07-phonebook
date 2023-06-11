b3736febd119b98df78d5a4539c8b6e7b86df742
Roman <vikulov.roman.work@gmail.com>
2023-06-11 14:47:32 +0300

    Update Filter.styled.js

--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -1,2 +1,15 @@
-export { FieldFilter, LabelFilter, InputFilter };
+import styled from 'styled-components';
+import { Field, Label, Input } from '../Form/Form.styled';
+
+const FieldFilter = styled(Field)``;
+
+const LabelFilter = styled(Label)`
+  font-size: 12px;
+`;
+
+const InputFilter = styled(Input)`
+  width: 250px;
+  padding: 5px 10px;
+`;
+
 export { FieldFilter, LabelFilter, InputFilter };
